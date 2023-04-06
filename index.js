8b8c77996a2544c5bb1d73c0166162faa9ca5a25
mrrsmburu <mrrsmburu@gmail.com>
2023-04-06 09:32:50 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+// declare global variable customerName
+var customerName = "bob";
+
+// function to uppercase customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+// function to set global variable bestCustomer
+function setBestCustomer() {
+   bestCustomer = "not bob";
+  return bestCustomer;
+}
+
+// function to overwrite global variable bestCustomer
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+  return bestCustomer;
+}
+
+// declare global constant leastFavoriteCustomer
+const leastFavoriteCustomer = "Karen";
+
+// function to attempt to change global constant leastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Bob";
+  // this will throw an error as constants cannot be reassigned
+}
